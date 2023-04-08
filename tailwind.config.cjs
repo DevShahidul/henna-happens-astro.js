90b54eeb44e58b96a37864191371f2be17fe22d7
shafiqahmad81 <shafiqahmad081@gmail.com>
2023-04-08 16:19:47 +0600

    update index file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,17 @@ module.exports = {
         black: {
           DEFAULT: '#383533',
           30: 'rgba(65, 60, 59, 0.3)',
+          3: 'rgba(0, 0, 0, 0.2)',
           button: '#0F0F0F'
         },
         light: '#413C3B',
         olive: '#A1BD74',
         disabled: '#CDCDCD',
+        gray:'#D9D9D9',
+        white:{
+          DEFAULT: '#ffffff',
+          7: 'rgba(255, 255, 255, 0.7)',
+        }
       },
       fontSize: {
         h1: '5rem',  // 80px
